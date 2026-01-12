9ced83299b797d4c0dea9daf04f75112e98e0830
Aaron Rendahl <rend0020@umn.edu>
2026-01-12 17:59:09 -0600

    finalize new functions

--- other2.R
+++ other2.R
@@ -6,20 +6,9 @@ library(xml2)
 conflicts_prefer(dplyr::filter, dplyr::lag)
 source("other2_functions.R")
 
-dat <- readxl::read_excel("sample.xlsx", "projects2") |>
-  mutate(across(c(startdate, enddate), as.Date)) |>
-  rename(prorate="prorate?") |>
-  mutate(prorate=(prorate |> str_sub(1, 1) |> replace_na("N"))=="Y")
+datx <- readxl::read_excel("sample.xlsx", "projects2") |>
+  prepare_projects()
 
-dat1 <- dat[2,]
+datx |> select(shorttitle, commitment) |> unnest(commitment)
 
-d1 <- dat1 |> select(shorttitle, !starts_with("year_"), -"prorate")
-d2 <- dat1 |> select(shorttitle, starts_with("year_"))
-
-prorate <- dat1$prorate
-date1 <- dat1$startdate
-date2 <- dat1$enddate
-effort <- d2 |> pivot_longer(starts_with("year_"), names_to=c("X", "year"), names_sep="_", values_to="effort") |>
-  select(year, effort) |> filter(!is.na(effort)) |> pull(effort)
-
-
+datx |> dat_to_xml() |> write_xml("sample2b.xml")
--- other2_functions.R
+++ other2_functions.R
@@ -21,12 +21,12 @@ get_effort <- function(date1, date2, effort, prorate) {
     m <- month(date1)
     ## don't need to prorate if started on first of the year...
     if(m==1) {
-      if(nrow(effort)!=length(years)) {
+      if(length(effort)!=length(years)) {
         stop("Need as many effort as years.")
       }
       effort1 <- tibble(year=years, effort=effort)
     } else {
-      if(nrow(effort)!=length(years)-1) {
+      if(length(effort)!=length(years)-1) {
         stop("Need as many effort as years.")
       }
       p <- (m-1)/12
@@ -34,5 +34,58 @@ get_effort <- function(date1, date2, effort, prorate) {
         mutate(effort=effort1*(1-p) + effort2*p)
     }
   }
-  effort1 |> select(years, effort)
+  effort1 |> select("year", "effort") |> 
+    mutate(effort=round(effort, 3))
+}
+
+add_commitment <- function(dat) {
+  row2list <- function(dat1) {
+    d1 <- dat1 |> select(shorttitle, !starts_with("year_"), -"prorate")
+    d2 <- dat1 |> select(shorttitle, starts_with("year_"))
+    ## grab the effort columns
+    effort <- d2 |> pivot_longer(starts_with("year_"), names_to=c("X", "year"), 
+                                 names_sep="_", values_to="effort") |>
+      select(year, effort) |> arrange(year) |> filter(!is.na(effort)) |> pull(effort)
+    ## if there is any effort, process it and add it to the data set
+    if(length(effort) > 0) {
+      ef <- get_effort(dat1$startdate, dat1$enddate, effort, dat1$prorate)
+      d1$commitment <- list(ef)
+    }
+    d1
+  }
+  bind_rows(lapply(seq_len(nrow(dat)), \(idx) row2list(dat[idx,])))
+}
+
+row_to_xml <- function(dat1) {
+  d1 <- dat1 |> select(-c("shorttitle", "commitment"))
+  ## make a list of the elements,
+  ## where each element has to be a list,
+  ## and missing values should be empty lists
+  d1.list <- lapply(d1, \(x) if(is.na(x)) list() else list(x))
+  if("commitment" %in% names(dat1)) {
+    d2 <- dat1$commitment[[1]]
+    d2.list <- map2(d2$year, d2$effort, \(y, e) {
+      list(personmonth = structure(list(e), year = y))
+    })
+    d1.list$commitment <- d2.list
+  }
+  list(support=d1.list)
+}
+
+dat_to_xml <- function(d) {
+  row_xml <- lapply(seq_len(nrow(d)), \(idx) row_to_xml(d[idx,]))
+  list(profile=list(
+    identification=list(),
+    employment=list(),
+    funding=row_xml
+  )) |> 
+    as_xml_document() 
+}
+
+prepare_projects <- function(d) {
+  d |> mutate(across(c(startdate, enddate), as.Date)) |>
+    rename(prorate="prorate?") |>
+    mutate(prorate=(prorate |> str_sub(1, 1) |> replace_na("N"))=="Y") |>
+    mutate(awardamount=as.integer(awardamount)) |>
+    add_commitment()
 }
