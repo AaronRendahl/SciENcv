045142e06526f7159abcd1debc1488b09822342b
Aaron Rendahl <rend0020@umn.edu>
2026-01-12 17:02:01 -0600

    move everything to functions

--- other_functions.R
+++ other_functions.R
@@ -1,9 +1,9 @@
 read_projects <- function(file, sheet="projects") {
   d2a <- readxl::read_excel(file, sheet) |> rename(var="internalcode")
   kb <- which(str_detect(d2a$var, "date$"))
-  d2b <- readxl::read_excel(support_xlsx, "projects", skip=kb[1], col_names=names(d2a), n_max = diff(range(kb))+1)
+  d2b <- readxl::read_excel(file, "projects", skip=kb[1], col_names=names(d2a), n_max = diff(range(kb))+1)
   kc <- which(str_detect(d2a$var, "^personmonths_[0-9]+$"))
-  d2c <- readxl::read_excel(support_xlsx, "projects", skip=kc[1], col_names=names(d2a), n_max = diff(range(kc))+1)
+  d2c <- readxl::read_excel(file, "projects", skip=kc[1], col_names=names(d2a), n_max = diff(range(kc))+1)
   xa <- d2a |> column_to_rownames("var") |> t() |> as_tibble()
   xb <- d2b |> column_to_rownames("var") |> t() |> as_tibble()
   xc <- d2c |> column_to_rownames("var") |> t() |> as_tibble()
@@ -12,17 +12,18 @@ read_projects <- function(file, sheet="projects") {
   xa$awardamount <- as.integer(xa$awardamount)
   xa
 }
-read_identification <- function(file, sheet="identification") {
-  name <- readxl::read_excel(file, sheet, "A1:B3", col_names=c("name", "value")) |>
-    column_to_rownames("name") |> t() |> as_tibble()
-  fix1 <- function(x) {c("name", paste0("Position_", 1:(length(x)-1)))}
-  positions <- readxl::read_excel(file, sheet, skip=5, col_names=FALSE, .name_repair = fix1) |>
-    column_to_rownames("name") |> t() |> as_tibble()
-  list(name=name, positions=positions)
-}
+
+# read_identification <- function(file, sheet="identification") {
+#   name <- readxl::read_excel(file, sheet, "A1:B3", col_names=c("name", "value")) |>
+#     column_to_rownames("name") |> t() |> as_tibble()
+#   fix1 <- function(x) {c("name", paste0("Position_", 1:(length(x)-1)))}
+#   positions <- readxl::read_excel(file, sheet, skip=5, col_names=FALSE, .name_repair = fix1) |>
+#     column_to_rownames("name") |> t() |> as_tibble()
+#   list(name=name, positions=positions)
+# }
 
 projects_to_xml <- function(dat) {
-  rowtolist <- function(d) {
+  rowtolist <- function(drow) {
     as.list1 <- function(x) {
       if(length(x)==1 & is.na(x)) {
         list()
@@ -30,13 +31,14 @@ projects_to_xml <- function(dat) {
         list(x)
       }
     }
-    com <- d |> mutate(row=1:n()) |> select(row, starts_with("personmonths_")) |>
-      pivot_longer(-row, names_to=c("X", "year"), names_sep="_") |>
+    
+    com <- drow |> select(starts_with("personmonths_")) |>
+      pivot_longer(everything(), names_to=c("X", "year"), names_sep="_") |>
       filter(!is.na(value))
     foo <- lapply(seq_len(nrow(com)), \(idx) {
       list(personmonth = structure(list(com$value[idx]), year = com$year[idx]))
     })
-    out <- d |> select(!starts_with("personmonths_")) |> lapply(as.list1)
+    out <- drow |> select(!starts_with("personmonths_")) |> lapply(as.list1)
     out$commitment <- foo
     out
   }
--- other_support.R
+++ other_support.R
@@ -2,24 +2,13 @@ library(conflicted)
 library(tidyverse)
 library(xml2)
 conflicts_prefer(dplyr::filter, dplyr::lag)
-
 source("other_functions.R")
 
-sample <- "1GsbLoMVysEDMR5CqYRJdnz1k6O1Y2BWvTCRHZ4Upi5E"
-support_xlsx <- "sample.xlsx"
 googledrive::drive_download(
-  googledrive::as_id(sample),
-  path=support_xlsx,
+  googledrive::as_id("1GsbLoMVysEDMR5CqYRJdnz1k6O1Y2BWvTCRHZ4Upi5E"),
+  path="sample.xlsx",
   overwrite=TRUE
 )
 
-#d1 <- read_identification(support_xlsx)
-d2 <- read_projects(support_xlsx)
-
-list(profile=list(
-  identification=list(),
-  employment=list(),
-  funding=projects_to_xml(d2)
-)) |> 
-  as_xml_document() |> write_xml("temp.xml")
+support_to_xml("sample.xlsx", "sample.xml")
 
