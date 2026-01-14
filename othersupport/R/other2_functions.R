9cb23e0af1e419631ddb2bf04876a4fe71ba76f9
Aaron Rendahl <rend0020@umn.edu>
2026-01-14 00:24:22 -0600

    big fixes!

--- othersupport/R/other2_functions.R
+++ othersupport/R/other2_functions.R
@@ -1,4 +1,23 @@
-process_effort <- function(date1, date2, effort, budget) {
+year_color <- "blue"
+
+## for simplicity, assume 30 days in every month...
+monthdiff <- function(x1, x2) {
+  (year(x2) - year(x1))*12 + month(x2) - month(x1) + 
+    (pmin(day(x2),30) - pmin(day(x1),30))/30
+}
+
+get_range <- function(date1, date2, budget) {
+  date1 <- as.Date(date1)
+  date2 <- as.Date(date2)
+  budget <- as.Date(budget)
+  if(day(date1) == day(date2+1)) { date2 <- date2 + 1 }
+  bx <- as.Date(sprintf("%d-%02d-%02d", (year(date1)-1):(year(date2)+1), month(budget), day(budget)))
+  bb1 <- max(bx[bx <= date1])
+  bb2 <- min(bx[bx >= date2])
+  bx[bx >= bb1 & bx <= bb2]
+}  
+
+process_effort <- function(date1, date2, effort, budget, daterange) {
   date1 <- as.Date(date1)
   date2 <- as.Date(date2)
   budget <- as.Date(budget)
@@ -24,11 +43,6 @@ process_effort <- function(date1, date2, effort, budget) {
   ## table of budget periods and effort
   ef <- tibble(b1=budgets[-length(budgets)], b2=budgets[-1], effort=effort)
 
-  ## for simplicity, assume 30 days in every month...
-  monthdiff <- function(x1, x2) {
-    (year(x2) - year(x1))*12 + month(x2) - month(x1) + 
-      (pmin(day(x2),30) - pmin(day(x1),30))/30
-  }
   monthmid <- function(x1, x2) {
     dif <- monthdiff(x1, x2)/2
     x1 + months(dif %/% 1) + days(round((dif %% 1)*30))
@@ -74,7 +88,6 @@ process_effort <- function(date1, date2, effort, budget) {
     mutate(eff=sapply(round(effort, 2), format),
            txt=sprintf("Year %d\n%s month%s", year, eff, if_else(eff=="1", "", "s")))
   
-  year_color <- "blue"
   effort_plot <- ggplot(ef_txt) + aes(xmin=b1, xmax=b2, ymin=0, ymax=efper) + 
     geom_rect(fill="gray90", color="gray50") +
     theme_minimal() +
@@ -86,13 +99,16 @@ process_effort <- function(date1, date2, effort, budget) {
     geom_vline(xintercept=as.Date(sprintf("%d-01-01", c(years, max(years)+1))),
                color=year_color, alpha=0.8, linetype="dashed") +
     geom_vline(xintercept=budget_range,
-               color="black", alpha=0.8, linetype="dotted") +
+               color="black", alpha=0.8, linetype="dashed", linewidth=1) +
     geom_text(aes(x=x, y=Inf, label=txt), size=6/.pt, vjust=1, hjust=0.5) +
     coord_cartesian(clip="off") +
-    expand_limits(x=budget_range) +
+    expand_limits(x=budget_range, y=1) +
     geom_text(aes(x=mid, y=0, label=txt), data=cal2_txt, vjust=-0.5, inherit.aes=FALSE, size=8/.pt, color=year_color) +
-    geom_text(aes(x=x, y=max(ef_txt$efper)/2, label=txt), data=cal_txt, inherit.aes=FALSE, size=10/.pt, color=year_color) +
+    geom_text(aes(x=x, y=max(c(ef_txt$efper, 1))/2, label=txt), data=cal_txt, inherit.aes=FALSE, size=10/.pt, color=year_color) +
     labs(x=NULL, y="Percent Effort")
+  if(!missing(daterange)) {
+    effort_plot <- effort_plot + expand_limits(x=daterange)
+  }
   list(calendar=cal, budget=ef, plot=effort_plot)
 }
 
@@ -102,11 +118,19 @@ required_vars <- c("shorttitle", "projecttitle", "awardnumber", "supportsource",
                    "supporttype")
 
 prepare_projects <- function(dat) {
-  dat <- dat |> mutate(awardamount=as.integer(awardamount))
+  dat <- dat |> mutate(awardamount=as.integer(awardamount)) |>
+    rename(budget="budget year start date") |>
+    mutate(budget=if_else(is.na(budget), startdate, budget))
+  
+  rr <- lapply(seq_len(nrow(dat)), \(idx) range(get_range(dat$startdate[[idx]], dat$enddate[[idx]], dat$budget[[idx]])))
+  rr1 <- range(do.call(c, rr))
+  rr2 <- range(as.Date(sprintf("%d-01-01",range(c(year(dat$startdate), year(dat$enddate-1)+1)))))
+  rr <- range(c(rr1, rr2))
+  
   row2list <- function(dat1) {
     d1 <- dat1 |> select(!starts_with("year "))
     d2 <- dat1 |> select(starts_with("year "))
-    budget_start <- dat1[["budget year start date"]]
+    budget_start <- dat1$budget
     if(is.na(budget_start)) {
       budget_start <- dat1$startdate
     }
@@ -114,9 +138,10 @@ prepare_projects <- function(dat) {
     effort <- d2 |> pivot_longer(starts_with("year "), names_to=c("X", "year"), 
                                  names_sep=" ", values_to="effort") |>
       select(year, effort) |> arrange(year) |> filter(!is.na(effort)) |> pull(effort)
+    
     ## if there is any effort, process it and add it to the data set
     if(length(effort) > 0) {
-      ef <- process_effort(dat1$startdate, dat1$enddate, effort, budget_start)
+      ef <- process_effort(dat1$startdate, dat1$enddate, effort, budget_start, daterange=rr)
       d1$commitment <- list(ef$calendar)
       d1$.budget <- list(ef$budget)
       d1$.plot <- list(ef$plot)
@@ -153,6 +178,13 @@ dat_to_xml <- function(d) {
 }
 
 all_effort_plot <- function(p) {
+  p <- p |> mutate(shorttitle=as_factor(shorttitle))
+  cal <- p |> select(shorttitle, commitment) |> unnest(commitment) |>
+    summarize(effort=sum(effort), .by=year) |>
+    mutate(x=as.Date(sprintf("%d-07-01", year))) |>
+    mutate(eff=sapply(round(effort, 2), format),
+           txt=sprintf("Year %d\n%s month%s", year, eff, if_else(eff=="1", "", "s")))
+  
   b <- p |> select(shorttitle, budget=.budget) |>
     unnest(budget) |> mutate(row=seq_len(n()), .before=1)
   dx <- sort(unique(c(b$b1, b$b2)))
@@ -160,25 +192,31 @@ all_effort_plot <- function(p) {
     mutate(d2=lead(d1), dif=monthdiff(d1, d2), 
            p=dif/sum(dif, na.rm=TRUE), 
            ef=effort*p,
+           efper=ef/dif,
            .by=row) |>
     filter(!is.na(d2))
   
+  ef_all_sum <- ef_all |> summarize(efper=sum(efper), ef=sum(ef), .by=c(d1, d2, dif)) |> arrange(d1)
+  my <- max(ef_all_sum$efper)
+    
   years <- sort(unique(year(dx)))
   ef_all_plot <- ef_all |> mutate(d2=d2-minutes(1)) |>
-    select(row, shorttitle, ddd_1=d1, ddd_2=d2, ef) |>
+    select(row, shorttitle, ddd_1=d1, ddd_2=d2, ef, efper) |>
     pivot_longer(starts_with("ddd_"), values_to="date")
-  pp <- ggplot(ef_all_plot) + geom_area(aes(x=date, y=ef, fill=shorttitle)) +
+  pp <- ggplot(ef_all_plot) + geom_area(aes(x=date, y=efper, fill=shorttitle)) +
     theme_minimal() +
     coord_cartesian(clip="off") +
     theme(legend.title = element_blank()) +
-    scale_y_continuous(expand=expansion(c(0,0.05)), labels=scales::percent) +
     labs(x=NULL, y="Percent Effort") +
     scale_x_date(breaks=as.Date(sprintf("%d-07-01", years)), labels=years,
                  minor_breaks=as.Date(as.vector(outer(years, c(1,4,7,10), \(y,m) sprintf("%d-%02d-01", y, m)))),
                  expand=expansion(mult=0.001)) +
-    scale_y_continuous(expand=expansion(mult=c(0.001,0.1)), labels=scales::percent) +
+    scale_y_continuous(expand=expansion(mult=c(0.001,0.15)), labels=scales::percent) +
     geom_vline(xintercept=as.Date(sprintf("%d-01-01", c(years, max(years)+1))),
-               color=year_color, alpha=0.8, linetype="dashed")
+               color=year_color, alpha=0.8, linetype="dashed") +
+    geom_text(aes(x=x, y=Inf, label=txt), data=cal, vjust=1,
+              inherit.aes=FALSE, size=10/.pt, color=year_color, fontface="bold") +
+    theme(axis.text.x = element_text(color=year_color))
   
   pp
 }
--- othersupport/app.R
+++ othersupport/app.R
@@ -15,53 +15,72 @@ library(xml2)
 library(shiny)
 conflicts_prefer(dplyr::filter, dplyr::lag)
 
-samplelink <- "https://docs.google.com/spreadsheets/d/1h3TJNpgWc5O1S7QEUeEePHlypPz27d9sCYfHFNuKNJ4/edit?usp=sharing"
+sampleurl <- "https://docs.google.com/spreadsheets/d/1h3TJNpgWc5O1S7QEUeEePHlypPz27d9sCYfHFNuKNJ4/edit?usp=sharing"
+samplelink <- tags$a(href=sampleurl, "View Sample Excel format", target="_blank")
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("SciENcv Other Support XML Conversion"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-          fileInput("upload", "Upload an Excel file with the right format:"),
-          "Then download the XML file:",
-          downloadButton("download"),
-          tags$p(tags$a(href=samplelink, "View Sample Spreadsheet", target="_blank"), style="margin-top:4em;")
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-          tableOutput("files"),
-          "Computed Person-Month Effort Per Year, Per Project:",
-          tableOutput("preview")
-        )
-    )
+  titlePanel("SciENcv Other Support XML Conversion"),
+  tags$style(HTML("div#plotId img {display: block; max-width: 100%;}")),
+  fluidRow(
+    column(6,
+           samplelink,
+           fileInput("upload", "Upload an Excel file with the right format:"),
+    ),
+    column(6,
+           tag("strong", "Then download the XML file:"),
+           downloadButton("download")
+    )),
+  fluidRow(
+    uiOutput("plot_all"),
+    uiOutput("plotUI")
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
   data <- reactive({
     req(input$upload)
-    readxl::read_excel(input$upload$datapath)
+    file <- input$upload$datapath
+    readxl::read_excel(file) |> prepare_projects()
   })
   
-    output$files <- renderTable(input$upload)
-    
-    output$preview <- renderTable({
-      datx <- data() |> prepare_projects()
-      datx |> select(shorttitle, commitment) |> unnest(commitment)
-    })
-    output$download <- downloadHandler(
-      filename = function() {
-        paste0(str_remove(input$upload$name, "\\.xlsx$"), ".xml")
-      },
-      content = function(file) {
-        data() |> prepare_projects() |> dat_to_xml() |> write_xml(file)
-      }
+  output$download <- downloadHandler(
+    filename = function() {
+      paste0(str_remove(input$upload$name, "\\.xlsx$"), ".xml")
+    },
+    content = function(file) {
+      data() |> dat_to_xml() |> write_xml(file)
+    }
+  )
+  
+  output$preview <- renderTable({
+    datx <- data() 
+    datx |> select(shorttitle, commitment) |> unnest(commitment)
+  })
+  
+  r <- 1/4
+  w <- 800  
+  
+  output$plotUI <- renderUI({
+    p <- data()
+    n <- nrow(p)
+    tagList(lapply(seq_len(n), \(i) {
+      sprintf("output %d/%d", i, n)
+      tagList(
+        h2(p$shorttitle[i]),
+        renderPlot(p$.plot[[i]], width=w, height=w*r, res=100)
+      )
+    }))
+  })
+  
+  output$plot_all <- renderUI({
+    p <- data()
+    tagList(
+      h2("All Combined"),
+      renderPlot(all_effort_plot(p), 
+                 width=w, height=w*r, res=100)
     )
+  })
+  
 }
 
 # Run the application 
