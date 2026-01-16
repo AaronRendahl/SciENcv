dc9b1954f0b115fcee4474d89741a773fb4f001e
Aaron Rendahl <rend0020@umn.edu>
2026-01-16 16:04:49 -0600

    don't include years with no effort

--- othersupport/R/other2_functions.R
+++ othersupport/R/other2_functions.R
@@ -184,7 +184,7 @@ row_to_xml <- function(dat1, current_year) {
   d1.list <- lapply(d1, \(x) if(is.na(x)) list() else list(x))
   if("commitment" %in% names(dat1)) {
     d2 <- dat1$commitment[[1]] |> mutate(effort=round(effort, 2)) |>
-      filter(year >= current_year)
+      filter(year >= current_year, effort > 0)
     d2.list <- map2(d2$year, d2$effort, \(y, e) {
       list(personmonth = structure(list(e), year = y))
     })
